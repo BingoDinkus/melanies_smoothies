85ebde09fe3cfb799a6f239f4738cbbb8c07ee9d
Eric Harlan <eric.j.harlan@outlook.com>
2024-07-20 15:22:13 -0400

    Connection changes needed due to move from SiS to SniS

--- streamlist_app.py
+++ streamlist_app.py
@@ -1,7 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col 
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -13,7 +12,8 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -25,10 +25,10 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ' '
-    
+
     for fruit_chosen in ingredients_list:
             ingredients_string += fruit_chosen + ' '
-    
+
     # st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -41,5 +41,5 @@ if ingredients_list:
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        
+
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
